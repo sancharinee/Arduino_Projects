0a9a982719d55aa76a439421b5dee10c488aa8e9
Sancharini Dey <67041010+sancharinee@users.noreply.github.com>
2022-04-20 13:38:29 +0530

    Add files via upload

---
+++ Projects/Edge Avaoiding Robot/EdgeAvaoidingRobot.ino
@@ -0,0 +1,52 @@
+#define LS 9 // left sensor
+#define RS 12 // right sensor 
+//definning Outputs
+#define LM1 8 // left motor 
+#define LM2 7 // left motor 
+#define RM1 4 // right motor 
+#define RM2 3 // right motor 
+
+void setup() 
+{ 
+  pinMode(LS, INPUT); 
+  pinMode(RS, INPUT); 
+  pinMode(LM1, OUTPUT); 
+  pinMode(LM2, OUTPUT); 
+  pinMode(RM1, OUTPUT); 
+  pinMode(RM2, OUTPUT);  
+}
+
+void loop() 
+{ 
+  if(digitalRead(LS) && digitalRead(RS)) // Move Forward 
+  { 
+    digitalWrite(LM1, HIGH); 
+    digitalWrite(LM2, LOW); 
+    digitalWrite(RM1, HIGH); 
+    digitalWrite(RM2, LOW); 
+  } 
+  
+  if(!(digitalRead(LS)) && digitalRead(RS)) // Turn right 
+  { 
+    digitalWrite(LM1, LOW); 
+    digitalWrite(LM2, HIGH); 
+    digitalWrite(RM1, HIGH); 
+    digitalWrite(RM2, LOW); 
+  } 
+  
+  if(digitalRead(LS) && !(digitalRead(RS))) // turn left 
+  { 
+    digitalWrite(LM1, LOW); 
+    digitalWrite(LM2, HIGH); 
+    digitalWrite(RM1, HIGH); 
+    digitalWrite(RM2, LOW); 
+  } 
+  
+  if(!(digitalRead(LS)) && !(digitalRead(RS))) // stop 
+  { 
+    digitalWrite(LM1, LOW); 
+    digitalWrite(LM2, LOW); 
+    digitalWrite(RM1, LOW); 
+    digitalWrite(RM2, LOW); 
+   }
+}
